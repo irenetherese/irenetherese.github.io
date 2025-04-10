0cab8bf77739f344bd75e8b52c474f803250ca9c
Thea <theabermejo@gmail.com>
2025-04-10 09:47:55 +0800

    Rebrand to green

--- my-website/src/app/projects/projects.component.ts
+++ my-website/src/app/projects/projects.component.ts
@@ -11,43 +11,50 @@ export class ProjectsComponent {
     {
       title: "Spark up Your Game",
       image: "/projects/bdcc/bdccbanner.png",
-      description: "Per Round CS:GO Predictions for Enhanced Sports Analytics \nCo-Authors: Raymundo Java Jr., Shem Harold Castillo, Karth Ian Macalalad",
+      description: "Per Round CS:GO Predictions for Enhanced Sports Analytics",
+      co_authors: ["Raymundo Java Jr.", "Shem Harold Castillo", "Karth Ian Macalalad"],
       link: "bdcc"
     },
     {
       title: "Feel Factor",
       image: "/projects/ml3/images/Title.PNG",
-      description: "Decoding Emotions Inside and Out \nCo-Authors: Kiana Alessandra Villaera, Julius Cris Salinas, Roderick Ilagan, Leonard Gail Rizada",
+      description: "Decoding Emotions Inside and Out",
+      co_authors: ["Kiana Alessandra Villaera", "Julius Cris Salinas", "Roderick Ilagan", "Leonard Gail Rizada"],
       link: "ml3"
     },
     {
       title: "Bet Smart",
       image: "/projects/ml2/title.png",
-      description: "Predicting VNL Match Outcomes Through Machine Learning \nCo-Authors: Arnica Joy Lacson, Raymundo Java Jr., James Ballesteros, Monico Ruben Marquez",
+      description: "Predicting VNL Match Outcomes Through Machine Learning",
+      co_authors: ["Arnica Joy Lacson", "Raymundo Java Jr.", "James Ballesteros", "Monico Ruben Marquez"],
       link: "ml2"
     },
     {
       title: "Machine Learning in Cryptocurrency Markets",
       image: "/projects/ml1/Title.PNG",
       description: "Investigating the Efficacy of Regression Algorithms in Timeseries Cryptocurrency Data",
+      co_authors: [],
       link: "ml1"
     },
     {
       title: "Decoding Chess Strategies",
       image: "/projects/adm/header1.png",
-      description: "Clustering Chess Openings For Enhanced Gameplay \nCo-Authors: Karth Ian Macalalad, Shem Harold Castillo, Raymundo Java Jr.",
+      description: "Clustering Chess Openings For Enhanced Gameplay",
+      co_authors: ["Karth Ian Macalalad", "Shem Harold Castillo", "Raymundo Java Jr."],
       link: "adm"
     },
     {
       title: "Data-Driven Victory",
       image: "/projects/dmw1/DMW1_banner.png",
-      description: "Feature Analysis using Dimensionality Reduction on CS:GO Player Profiles \nCo-Authors: Raymundo Java Jr., James Ballesteros, Merv Derek Camado",
+      description: "Feature Analysis using Dimensionality Reduction on CS:GO Player Profiles",
+      co_authors: ["Raymundo Java Jr.", "James Ballesteros", "Merv Derek Camado"],
       link: "dmw1"
     },
     {
       title: "Steam Odyssey",
       image: "/projects/dmw2/TITLE.PNG",
-      description: "Building a Content-based Recommender System based on Game and User Profiles \nCo-Authors: Rafael Nable, Jamil Hadji Alawi, Karlmarx Rubia, Earl John Gallarde",
+      description: "Building a Content-based Recommender System based on Game and User Profiles",
+      co_authors: ["Rafael Nable", "Jamil Hadji Alawi", "Karlmarx Rubia", "Earl John Gallarde"],
       link: "dmw2"
     }
   ];
